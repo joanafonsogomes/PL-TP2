cccf4ce38f86881df4fa59f05124774eac37f9c0
SusanaMarques99 <susymarques99@gmail.com>
2021-05-22 19:39:16 +0100

    somethings done

--- gerador_lex.py
+++ gerador_lex.py
@@ -1,14 +1,25 @@
 import ply.lex as lex
 
-tokens = ['num','id']
-literals = ['(',')','+','-','*','/','?','!','=']
+reserved = {
+    'if' : 'IF',
+    'then' : 'THEN',
+    'else' : 'ELSE',
+    'for' : 'FOR',
+    'fi' : 'FI',
+    'rof' : 'ROF'
+ }
+tokens = ['num','id'] + list(reserved.values())
+literals = ['(',')','>','<','+','-','*','/','?','!',';','=']
 
+def t_id(t):
+     r'[a-z][a-zA-Z_0-9]*'
+     t.type = reserved.get(t.value,'id')    # Check for reserved words
+     return t
 t_num = r'\d+'
-t_id = r'[a-z]' #apenas 26 registos
 t_ignore = "\t\n"
 
 def t_error(t):
-    print("Carater ilegal: ", t.value[0])
+    #print("Carater ilegal: ", t.value[0])
     t.lexer.skip(1)
 
 #build the lexer
--- gerador_yacc.py
+++ gerador_yacc.py
@@ -1,118 +1,177 @@
 import ply.yacc as yacc 
+import fileinput
 import sys
-
 from gerador_lex import tokens 
 
 
 #Production rules
 def p_Comandos(p):
     "Comandos : Comandos Comando"
-    pass
-
+    p[0] = p[1] + p[2]
 def p_Comando_unico(p):
     "Comandos : Comando"
-    pass
+    p[0] = p[1]
 
 def p_Comando(p):
     """
     Comando : Ler
            |  Escrever
-           |  Despejar
            |  Atrib
            |  Exp
            |  End
+           |  Condition
+           |  Cicle
     """
-    pass
-    
+    p[0] = p[1]  
+
+ 
 
 def p_Exp_add(p):
     "Exp : Exp '+' Termo" 
-    p[0] = p[1] + p[3]
-    print(p[0])
-    ger = 'PUSHI ' + str(p[1])+ '\n'+ 'PUSHI ' +str(p[3])+ '\n'+ 'ADD\n'
-    file_vm.write(ger)
+    p[0] = p[1]+ p[3]+ 'ADD\n'
+    # file_vm.write(p[0])
 
 def p_Exp_sub(p):
     "Exp : Exp '-' Termo" 
-    p[0] = p[1] - p[3]
-    print(p[0])
-    ger = 'PUSHI ' + str(p[1])+ '\n'+ 'PUSHI ' +str(p[3])+ '\n'+ 'SUB\n'
-    file_vm.write(ger)
+    p[0] = p[1]+ p[3]+ 'SUB\n'
+    # file_vm.write(p[0])
 
 def p_Exp_termo(p):
     "Exp : Termo"
     p[0] = p[1]
+    # file_vm.write(p[0])
 
 def p_Termo_mul(p):
     "Termo : Termo '*' Factor"
-    p[0] = p[1] * p[3]
-    print(p[0])
-    ger = 'PUSHI ' + str(p[1])+ '\n'+ 'PUSHI ' +str(p[3])+ '\n'+ 'MUL\n'
-    file_vm.write(ger)
+    p[0] = p[1]+ p[3] + 'MUL\n'
+    # file_vm.write(p[0])
+
 def p_Termo_div(p):
     "Termo : Termo '/' Factor"
     if(p[3] != 0):
-        p[0] = p[1] / p[3]
-        print(p[0])
-        ger = 'PUSHI ' + str(p[1])+ '\n'+ 'PUSHI ' +str(p[3])+ '\n'+ 'DIV\n'
-        file_vm.write(ger)
+        p[0] = p[1]+ p[3]+ 'DIV\n'
+        # file_vm.write(p[0])
     else:
         print ("Erro: divisao por 0, a continuar com 0...)")
-        p[0] =0
+        p[0] = str(0)
+        # file_vm.write(p[0])
 
 def p_Termo_factor(p):
     "Termo : Factor"
     p[0] = p[1]
+    # file_vm.write(p[0])
 
 def p_Factor_id(p):
     "Factor : id"
-    p[0] = p.parser.registers.get(p[1])
+    indice = p.parser.registers.get(p[1])
+    p[0] = 'PUSHG ' + str(indice) + '\n'
 
 def p_Factor_num(p):
     "Factor : num"
-    p[0] = int(p[1])
+    p[0] = 'PUSHI ' + p[1] + '\n'
+    # file_vm.write(p[0])
 
 def p_Factor_group(p):
-    "Factor : '(' Exp ')' "
+    "Factor : '(' Exp ')'"
     p[0] = p[2]
+    # file_vm.write(p[0])
 
+##############WORKING ON IT#####################3
 
 def p_Ler(p):
+    ##falta ler da vm e nao do python!!!
     "Ler : '?' id"
     global i
     flag=1
     valor = input("Introduza o valor inteiro: ")
     for key in p.parser.registers.keys():
         if key == p[2]:
-            ger = 'PUSHI 0\nPUSHI ' +valor+ '\n'+ 'STOREG '+ str(p.parser.registers.get(key)) +'\n'
+            p[0] = 'PUSHI ' +valor+ '\n'+ 'STOREG '+ str(p.parser.registers.get(key)) +'\n'
             flag=0
-            file_vm.write(ger)
+            file_vm.write(p[0])
     if(flag==1):
         p.parser.registers.update({p[2]: i})
-        ger = 'PUSHI 0\nPUSHI ' +valor+ '\n'+ 'STOREG '+ str(i) +'\n'
+        p[0] = 'PUSHI 0\nPUSHI ' +valor+ '\n'+ 'STOREG '+ str(i) +'\n'
         i= i+1
-        file_vm.write(ger)
+        # file_vm.write(p[0])
 
 def p_Escrever(p):
     "Escrever : '!' id"
     for key in p.parser.registers.keys():
         if key == p[2]:
-            ger= 'PUSHG ' +str(p.parser.registers.get(key)) +'\nWRITEI\n'
-            file_vm.write(ger)
+            p[0]= 'PUSHG ' +(p.parser.registers.get(key)) +'\nWRITEI\n'
+            # file_vm.write(p[0])
 
-def p_Despejar(p):
-    "Despejar : '!' '!'"
-    print(p.parser.registers)
-
+##############wORKING UNTIL HERE################
 def p_Atrib(p):
     "Atrib : id '=' Exp"
-    p.parser.registers.update({p[1]: p[3]})
-    
+    global i
+    flag=1
+    for key in p.parser.registers.keys():
+            if key == p[1]:
+                p[0] = p[3] + 'STOREG '+ str(p.parser.registers.get(key)) +'\n'
+                flag=0
+                # file_vm.write(p[0])
+    if(flag==1):
+            p.parser.registers.update({p[1]: i})
+            p[0] = 'PUSHI 0\n' +p[3]+ 'STOREG '+ str(i) +'\n'
+            i= i+1
+            # file_vm.write(p[0])
+
+def p_Condition_maior(p):
+    "Condition : IF '(' Exp '>' Termo ')' THEN Comandos ELSE Comandos FI"
+    # file_vm.write(p[0])
+
+    p[0] = p[3] + p[5] +'SUP\n' +'JZ else\n' + (p[8])+ 'JUMP fim\n' + 'else:\n' + (p[10])+ 'fim:\n'
+    # file_vm.write(p[0])
+
+def p_Condition_menor(p):
+    "Condition : IF '(' Exp '<' Termo ')' THEN Comandos ELSE Comandos FI" 
+    p[0] = p[3] + p[5] +'INF\n' +'JZ else\n' + p[8]+'JUMP fim\n' + 'else:\n' + p[10] + 'fim:\n'
+    # file_vm.write(p[0])
+
+def p_Condition_maiorigual(p):
+    "Condition : IF '(' Exp '>' '=' Termo ')' THEN Comandos ELSE Comandos FI"
+    p[0] = p[3] + p[6]+ 'SUPEQ\n' +'JZ else\n' + p[9]+'JUMP fim\n'  + 'else:\n' + p[11]+ 'fim:\n' 
+    # file_vm.write(p[0])
+
+def p_Condition_menorigual(p):
+    "Condition : IF '(' Exp '<' '=' Termo ')' THEN Comandos ELSE Comandos FI"
+    p[0] = p[3] + p[6] +'INFEQ\n' +'JZ else\n' + p[9]+'JUMP fim\n'  + 'else:\n' + p[11] + 'fim:\n'
+    # file_vm.write(p[0])
+
+def p_Condition_igual(p):
+    "Condition : IF '(' Exp '=' '=' Termo ')' THEN Comandos ELSE Comandos FI"
+    p[0] = p[3] + p[6] +'EQUAL\n' +'JZ else\n' + p[9]+'JUMP fim\n'  + 'else:\n' + p[11] + 'fim:\n'
+    # print(p[0])     
+    # file_vm.write(p[0])
+
+def p_Cicle_menor(p):
+    "Cicle : FOR '(' Atrib ';' Factor '<' Factor ';' Atrib ')' Comandos ROF"
+    p[0] = p[3] + 'ciclo1:\n' + p[5] + p[7] + 'INF\n'  +'JZ fim\n' + p[11] + p[9]  + 'JUMP ciclo1\n' + 'fim:\n' 
+
+def p_Cicle_maior(p):
+    "Cicle : FOR '(' Atrib ';' Factor '>' Factor ';' Atrib ')' Comandos ROF"
+    p[0] = p[3] + 'ciclo1:\n' + p[5] + p[7] + 'SUP\n'  +'JZ fim\n' + p[11] + p[9]  + 'JUMP ciclo1\n' + 'fim:\n' 
+
+def p_Cicle_menor_igual(p):
+    "Cicle : FOR '(' Atrib ';' Factor '<''=' Factor ';' Atrib ')' Comandos ROF"
+    p[0] = p[3] + 'ciclo1:\n' + p[5] + p[7] + 'INFEQ\n'  +'JZ fim\n' + p[11] + p[9]  + 'JUMP ciclo1\n' + 'fim:\n' 
+
+def p_Cicle_maior_igual(p):
+    "Cicle : FOR '(' Atrib ';' Factor '<' Factor ';' Atrib ')' Comandos ROF"
+    p[0] = p[3] + 'ciclo1:\n' + p[5] + p[7] + 'SUPEQ\n'  +'JZ fim\n' + p[11] + p[9]  + 'JUMP ciclo1\n' + 'fim:\n' 
+
+
+def p_Cicle_igual(p):
+    "Cicle : FOR '(' Atrib ';' Factor '<' Factor ';' Atrib ')' Comandos ROF"
+    p[0] = p[3] + 'ciclo1:\n' + p[5] + p[7] + 'EQUAL\n'  +'JZ fim\n' + p[11] + p[9]  + 'JUMP ciclo1\n' + 'fim:\n' 
+
 
 def p_End(p):
     "End : '!' '!' '!'"
-    # dá erro fp=sp, os apontadores vao para o mesmo sitio
-    file_vm.write('STOP')
+    # da erro fp=sp, os apontadores vao para o mesmo sitio
+    p[0]='STOP'
     file_vm.close()
 
 #Error value for syntax errors
@@ -125,7 +184,7 @@ def p_error(p):
                   stack_state_str,
                   p))
 
-#Build the parser
+          #Build the parser
 parser =  yacc.yacc()
 # my state
 # dicionario inicializado a vazio
@@ -140,6 +199,7 @@ file = 'file.vm'
 file_vm = open(file, "w+")
 file_vm.write('START\n')
 # reading input
-for linha in sys.stdin:
+for linha in fileinput.input():
     result = parser.parse(linha)
+    file_vm.write(str(result))
 
